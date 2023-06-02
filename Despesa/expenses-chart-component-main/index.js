d04f04d429c2a1b8accb9cc9fe70cb57ba8e7ba3
Pedro Neves <111977651+PedroN3ves@users.noreply.github.com>
2023-06-02 10:12:18 -0300

    Add files via upload

---
+++ Despesa/expenses-chart-component-main/index.js
@@ -0,0 +1,48 @@
+const data = [
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    },
+];
+
+const week = document.querySelector('.week')
+
+window.addEventListener('DOMContentLoaded', () =>{
+    
+    let displayWeek = data.map(  (item) =>{
+        return`
+        <div class="day">
+          <div class=${item.day}></div>
+          <article><p>${item.day}</p></article>
+        </div>
+        `
+    })
+    displayWeek = displayWeek.join('')  
+    console.log(displayWeek)
+
+    week.innerHTML = displayWeek
+})
